dff352f16acf1ec19b6899f8deaf724028f5ea1e
Aelida <lidasouza89@gmail.com>
2022-04-24 19:49:39 -0300

    Inclusão da Resolução Requisito 6

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-window.onload = document.getElementsByClassName('color')[0].style.background = "black";
-window.onload = document.getElementsByClassName('color')[1].style.background = "yellow";
-window.onload = document.getElementsByClassName('color')[2].style.background = "red";
-window.onload = document.getElementsByClassName('color')[3].style.background = "blue";
+let colorsDiv = document.getElementsByClassName('color');
+let colors = ['black','yellow','red','blue']
+for (let i = 0; i < colors.length; i++){
+    colorsDiv[i].style.backgroundColor = colors[i];
+}
+
 
 
 function criarQuadrado() {
 
     let pixelBoard = document.getElementById('pixel-board');
-    
-for (let linha = 0; linha < 5; linha++){
-    let divLinha = document.createElement('div')
-    
-    for(let coluna = 0; coluna < 5; coluna++){
-        let pixels = document.createElement('div');
-        divLinha.appendChild(pixels);
-        pixels.className= "pixel";
+
+    for (let linha = 0; linha < 5; linha++) {
+        let divLinha = document.createElement('div')
+
+        for (let coluna = 0; coluna < 5; coluna++) {
+            let pixels = document.createElement('div');
+            divLinha.appendChild(pixels);
+            pixels.className = "pixel";
+        }
+
+        pixelBoard.appendChild(divLinha);
+
     }
 
-    pixelBoard.appendChild(divLinha);
-
-    
-
-
-
-
 }
+criarQuadrado();
 
+function obterCor (elemento){
+
+    return elemento.style.backgroundColor;
 }
-criarQuadrado()
+let corAtual = obterCor(colorsDiv[0]);
+
+
